d20eacb9a98c1380b080b49b104d03738f5319b8
Jean <jeangfds@gmail.com>
2022-01-20 01:23:14 -0300

    atualizar

--- Projetos/Carrinho_compras.dart
+++ Projetos/Carrinho_compras.dart
@@ -2,9 +2,28 @@ import 'dart:io';
 
 main() {
   List<String> produtos = [];
-while(){
-  
-}
-  String texto = stdin.readLineSync()!;
-
+  bool condicao = true;
+  while (condicao) {
+    print("-------Adicone o Produto---------");
+    String texto = stdin.readLineSync()!;
+    if (texto == "sair") {
+      print("------TERMINOU O PROGRAMA----------");
+      condicao = false;
+    } else if (texto == "imprimir") {
+      for (var i = 0; i < produtos.length; i++) {
+        print("ITEM $i-${produtos[i]}");
+      }
+    } else if (texto == "remover") {
+      print("-------Remover Produto---------");
+      for (var i = 0; i < produtos.length; i++) {
+        print("ITEM $i-${produtos[i]}");
+      }
+      int item = int.parse(stdin.readgit LineSync()!);
+      produtos.removeAt(item);
+      print("-------Produto Removido---------");
+    } else {
+      produtos.add(texto);
+      print("\x1B[2J\x1B[0;0H");
+    }
+  }
 }
